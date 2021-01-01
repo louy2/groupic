08b473f4f2851d0097383cfffaf93d98b23fd1d0
Yufan Lou <2263580+louy2@users.noreply.github.com>
2021-01-01 01:01:32 -0500

    Add avatar command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,78 @@
-fn main() {
-    println!("Hello, world!");
+use serenity::async_trait;
+use serenity::client::{Client, Context, EventHandler};
+use serenity::framework::standard::{
+    macros::{command, group, hook},
+    CommandResult, StandardFramework,
+};
+use serenity::model::channel::Message;
+
+use std::env;
+use tracing::{error, info};
+use tracing_subscriber;
+
+#[group]
+#[commands(ping, avatar)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .pretty()
+        .with_thread_names(true)
+        // enable everything
+        .with_max_level(tracing::Level::INFO)
+        // sets this to be the default, global collector for this application.
+        .init();
+
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .before(log_command_user)
+        .group(&GENERAL_GROUP);
+
+    // Login with a bot token from the environment
+    let token = env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN env var not found, cannot log in");
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[hook]
+#[instrument(level = "debug")]
+async fn log_command_user(_: &Context, msg: &Message, command_name: &str) -> bool {
+    info!(
+        "Got command '{}' by user '{}'",
+        command_name, msg.author.name
+    );
+
+    true
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    if let Err(why) = msg.reply(ctx, "Pong!").await {
+        error!("Error sending message {:?}", why)
+    }
+
+    Ok(())
+}
+
+#[command]
+async fn avatar(ctx: &Context, msg: &Message) -> CommandResult {
+    if let Err(why) = msg.reply(ctx, msg.author.static_face()).await {
+        error!("Error sending message {:?}", why)
+    }
+
+    Ok(())
 }
